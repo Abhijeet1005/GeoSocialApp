fcefab71c870fce797401385b5a7a045a90e28af
Abhijeet Chauhan <aschauhan1052002@gmail.com>
2024-01-08 19:26:42 +0530

    Post creation now working properly

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const passport = require('passport')
 const localStrategy = require('passport-local') 
 const userModel = require('./users')
+const postModel = require('./posts')
 passport.use(new localStrategy(userModel.authenticate()))
 
 
@@ -15,18 +16,32 @@ router.get('/',function(req,res,next){
 router.get('/login', function(req,res){
   res.render('login',{messages: req.flash()})
 })
-
 router.post('/login',passport.authenticate('local',{
   successRedirect: "/profile",
   failureRedirect: "/login",
   failureFlash: true 
-  
 }),function(req,res){
 });
 
 //profile routes
 router.get('/profile',isLoggedIn,function(req,res){
   res.render('profile')
+});
+
+//Post creation route
+router.post('/post',async function(req,res,next){
+  //fetch user -> create a post with that user ID and the data provided through form -> save that and then get req at /profile
+  const user = await userModel.findOne({
+    username: req.session.passport.user
+  })
+  const newPost = await postModel.create({
+    user: user._id,
+    postCaption: req.body.caption,
+    postGeo: req.body.location,
+  })
+  user.posts.push(newPost._id)
+  await user.save()
+  res.redirect('/profile')
 })
 
 // register routes
@@ -42,7 +57,7 @@ router.post('/register', function(req,res){
       res.redirect('/profile')
     })
   })
-})
+});
 
 //logout route
 router.get('/logout',function(req,res){
